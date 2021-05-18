9b7a2214bda24eb35296df609bfa88785f5201a2
Felipe Marinho <felipmarinho@gmail.com>
2021-05-18 05:14:19 -0300

    implementando a função findUserById

--- src/index.js
+++ src/index.js
@@ -32,7 +32,13 @@ function checksTodoExists(request, response, next) {
 }
 
 function findUserById(request, response, next) {
-  // Complete aqui
+  const { id } = request.params;
+  const user = users.find(user => user.id === id);
+  if (!user) {
+    return response.status(404).json({ mensagem: '' })
+  }
+  request.user = user;
+  return next();
 }
 
 app.post('/users', (request, response) => {

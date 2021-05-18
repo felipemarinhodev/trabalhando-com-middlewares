374d096df1d1a3305ccff66c429129e25c04eb00
Felipe Marinho <felipmarinho@gmail.com>
2021-05-18 05:31:09 -0300

    implementando o middleware checksTodoExists

--- src/index.js
+++ src/index.js
@@ -28,14 +28,33 @@ function checksCreateTodosUserAvailability(request, response, next) {
 }
 
 function checksTodoExists(request, response, next) {
-  // Complete aqui
+  const { username } = request.headers;
+  const user = users.find(user => user.username === username);
+  if (!user) {
+    return response.status(404).json({ mensagem: ''});
+  }
+
+  const { id } = request.params;
+  if (!validate(id)) {
+    return response.status(400).json({ mensagem: ''});
+  }
+
+  const hasTodo = user.todos.some(todo => todo.id === id);
+  if (!hasTodo) {
+    return response.status(404).json({ mensagem: ''});
+  }
+  const todo = user.todos.find(todo => todo.id === id);
+  
+  request.user = user;
+  request.todo = todo;
+  return next();
 }
 
 function findUserById(request, response, next) {
   const { id } = request.params;
   const user = users.find(user => user.id === id);
   if (!user) {
-    return response.status(404).json({ mensagem: '' })
+    return response.status(404).json({ error: '' })
   }
   request.user = user;
   return next();
